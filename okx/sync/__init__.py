b0f1e9744520da0eb6a3fb827652efbee47313c6
wangyundlut <wangyundlut@126.com>
2022-03-21 22:53:30 +0800

    this is a test

--- okx/sync/__init__.py
+++ okx/sync/__init__.py
@@ -4,3 +4,4 @@
 # from exceptions import OKXRequestException, OKXAPIException, OKXParamsException
 # from rest_v5 import okx_api_v5
 # from utils import sign, pre_hash, get_header, parse_params_to_str, get_timestamp, signature, okx_timestamp_v5
+# add test
